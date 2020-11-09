f8ef6117ce89da73e0bcd212305d8efa2e560790
Joel Gonzalez <espjoemusic@gmail.comgir config --global user.name JoelGonzalez02>
2020-11-08 18:55:05 -0800

    mvp completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,16 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
+def get_indices_of_item_weights(weights, length, limit):
+ 
+    HashTable = {}
+
+    for i in range(length):
+        target = limit - weights[i]
+
+        if target in HashTable.values():
+            x = i
+            y = [k for k,v in HashTable.items() if v == target][0]
+            return (x, y) if x > y else (y, x)
+
+        HashTable[i] = weights[i]
+    
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,29 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    """
+    1. create an empty hash table and an empty list 'route'
+    2. loop through tickets and store the source and destination
+    as a key/value pair in the hash table
+    3. lookup the flight with source 'NONE'
+    4. starting with this flight, push the destination into 'route',
+    then find each subsequent flight, pushing the destination into 'route'
+    each time
+    5. once you reach the flight with destination 'NONE', you're finished
+    """
+
+    HashTable = {}
+    route = []
+
+    for t in tickets:
+        HashTable[t.source] = t.destination
+
+    location = HashTable['NONE']
+    while location != 'NONE':
+        route.append(location)
+        location = HashTable[location]
+
+    route.append('NONE')
+
 
     return route
